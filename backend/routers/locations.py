80eaf4a72c3249e1306132582175921a8fcbb19a
Anton Kriese <anton.kriese@gmail.com>
2023-05-02 16:36:06 -0400

    docs(api): add some thoughts about error handling

--- backend/routers/locations.py
+++ backend/routers/locations.py
@@ -44,10 +44,20 @@ def create_new_location(new_data: dict):
 
 @router.put("/{location_id}")
 def update_location(location_id: int):
+    # error if location not found
+
+    # error if user has no rights to update location
+
+    # error if data is incomplete
+
     pass
 
 @router.delete("/{location_id}")
 def delete_location(location_id: int):
+    # error if location not found
+
+    # error if user has no rights to remove location
+
     pass
 
 #### REVIEWS
@@ -59,22 +69,49 @@ review_router = APIRouter(
 
 @review_router.post("/")
 def create_review(location_id: int, data: dict):
+    # error if location not found
+
+    # error if user has review for location already
+
+    # error if data is incomplete or incorrect
+
     pass
 
 @review_router.put("/")
-def update_review(location_id: int, data: dict):
+def update_review(location_id: int, review_id: int, data: dict):
+    # error if location not found
+
+    # error if review doesnt exist
+
+    # error if review isnt owned by user
+
+    # error if data is inclomplete or incorrect
+
+    # error if location and review dont match
     pass
 
 @review_router.put("/confirmation")
 def set_confirmation(location_id: int, data: dict):
+    # error if location not found
+
+    # error if user has same confirmation for location already
+
+    # error if data is inclomplete or incorrect
+
     pass
 
 @review_router.delete("/")
 def remove_review(location_id: int):
+    # error if location not found
+
     pass
 
 @review_router.put("/{review_id}")
 def report_review(location_id: int, review_id: int, reason: str):
+    # error if location not found
+
+    # error if review does not exist
+
     pass
 
 router.include_router(review_router)
