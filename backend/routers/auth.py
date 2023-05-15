3e1976ea6e5fee76f4db001a4b0e24d7de3d0cd0
Anton Kriese <anton.kriese@gmail.com>
2023-05-14 23:27:58 -0300

    feat(auth): get_current_user as dependency func
    this can be injected for all endpoints that need for the user to have
    active session

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -35,3 +35,19 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     token = create_access_token(data={ "sub": form_data.username })
 
     return { "access_token": token, "token_type": "bearer" }
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        token_data = decode_token(token)
+    except:
+        raise HTTPException(400, "Invalid token!")
+
+    user = await get_user_by_name(token_data.username)
+    if user is None:
+        raise HTTPException(400, "User not found!")
+
+    return user
+
+@router.get("/users/me")
+async def get_me(user: Annotated[User, Depends(get_current_user)]) -> UserAPI:
+    return UserAPI(**user.dict())
