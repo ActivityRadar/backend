db8fd8a0a8194cf4309838961cf790007cffcf13
Anton Kriese <anton.kriese@gmx.de>
2023-07-25 10:56:46 +0200

    feat: create json file with openapi scheme

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
+import json
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
 from .database.connection import init as init_db
 from .routers import admin, auth, chats, locations, offers, users
@@ -26,3 +29,16 @@ async def startup():
     app.include_router(offers.router)
     app.include_router(chats.router)
     # app.include_router(events.router)
+
+    with open("openapi.json", "w") as f:
+        json.dump(
+            get_openapi(
+                title=app.title,
+                version=app.version,
+                openapi_version=app.openapi_version,
+                description=app.description,
+                routes=app.routes,
+                # openapi_prefix=app.openapi_prefix,
+            ),
+            f,
+        )
