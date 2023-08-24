c83e030926866471f58cb359d6bc6f5f72f4fd0a
Anton Kriese <anton.kriese@gmx.de>
2023-08-24 18:55:04 +0200

    feat!: rework offer schemas to add location blurr

--- backend/database/service/offers.py
+++ backend/database/service/offers.py
@@ -1,16 +1,17 @@
+import math
+import random
 from datetime import datetime
 
 from beanie import PydanticObjectId
 from beanie.odm.operators.find import BaseFindOperator
-from beanie.odm.operators.find.comparison import Eq, In, NotIn
-from beanie.operators import Near, Not
+from beanie.odm.operators.find.comparison import Eq, In
+from beanie.odm.queries.aggregation import AggregationQuery
+from beanie.operators import Near, Push
+from geopy import distance as geo_distance
 
-from backend.database.models.locations import (
-    LocationDetailedDb,
-    LocationShort,
-    LocationShortDb,
-)
+from backend.database.models.locations import LocationDetailedDb, LocationShortDb
 from backend.database.models.offers import (
+    LocationBlurrOut,
     Offer,
     OfferCreatorInfo,
     OfferIn,
@@ -19,7 +20,10 @@ from backend.database.models.offers import (
     OfferTime,
     OfferType,
     OfferVisibility,
+    Participant,
+    ParticipantStatus,
 )
+from backend.database.models.shared import GeoJsonLocation
 from backend.database.models.users import User
 from backend.util import errors
 from backend.util.types import LongLat, TimeSlotFixed
@@ -42,10 +46,29 @@ class TimesMatcher:
         return s1 <= s2 <= e1 or s2 <= s1 <= e2
 
 
+def blurr_center(center: GeoJsonLocation, radius: float):
+    radius_meters = radius * 1000
+    rand_radius = math.sqrt(random.uniform(0, 1)) * radius_meters
+    angle = 2 * math.pi * random.random()
+
+    long, lat = center.coordinates
+    meters_per_degree = 111000.0
+    lat_adjustment = rand_radius / meters_per_degree * math.cos(angle)
+    long_adjustment = (
+        rand_radius
+        / (meters_per_degree * math.cos(math.radians(long)))
+        * math.sin(angle)
+    )
+
+    return GeoJsonLocation(
+        type="Point", coordinates=[long + long_adjustment, lat + lat_adjustment]
+    )
+
+
 class OfferService:
     tm = TimesMatcher()
 
-    async def create(self, user: User, offer: OfferIn) -> PydanticObjectId:
+    async def create(self, user: User, offer: OfferIn) -> Offer:
         # TODO: check if user is eligible to create an offer.
 
         location = offer.location
@@ -55,14 +78,25 @@ class OfferService:
                 raise errors.LocationDoesNotExist()
             location.coords = loc.location
 
+        participants = [Participant(id=user.id, status=ParticipantStatus.HOST)]
+
+        blurr = LocationBlurrOut(
+            radius=offer.blurr.radius,
+            center=blurr_center(
+                center=offer.location.coords, radius=offer.blurr.radius
+            ),
+        )
+
         new_offer = await Offer(
             creation_date=datetime.utcnow(),
             user_info=OfferCreatorInfo(**user.dict()),
             status=OfferStatus.OPEN,
+            participants=participants,
+            blurr_info=blurr,
             **offer.dict(),
         ).insert()
 
-        return new_offer.id
+        return new_offer
 
     async def _check_and_set_timeout(self, offers) -> list[Offer]:
         now = datetime.utcnow()
@@ -112,12 +146,15 @@ class OfferService:
         # Or(Eq(Offer.visibility, OfferVisibility.PUBLIC),
         # And(Eq(Offer.visibility, OfferVisibility.FRIENDS))),
 
-        offers = await Offer.find(*filters).to_list()
+        offers = await Offer.find_many(*filters).to_list()
         offers = await self._check_and_set_timeout(offers)
 
         return offers
 
-    async def get(self, ids: list[PydanticObjectId]) -> list[Offer]:
+    async def get(self, id: PydanticObjectId) -> Offer | None:
+        return await Offer.get(id)
+
+    async def get_bulk(self, ids: list[PydanticObjectId]) -> list[Offer]:
         offers = await Offer.find(In(Offer.id, ids)).to_list()
 
         return offers
@@ -141,12 +178,14 @@ class OfferService:
         self,
         user: User,
         center: LongLat,
-        distance: float,  # in meters
+        distance: float,  # in km
         time: OfferTime,
         activities: list[str] | None,
     ) -> list[Offer]:
         filters: list[BaseFindOperator] = [
-            Near(Offer.location.coords, *center, max_distance=distance),
+            # use this: when beanie/#674 is fixed!
+            # Near(Offer.blurr_info.center, *center, max_distance=distance * 1000),
+            Near("blurr_info.center", *center, max_distance=distance * 1000),
         ]
 
         if activities:
@@ -154,7 +193,17 @@ class OfferService:
 
         offers = await self._get_with_filters(user, filters)
 
-        return self._filter_date_time(offers, time)
+        # TODO: filter out offers with too low visibility radius
+        # maybe as in https://stackoverflow.com/questions/28659081/mongodb-near-geonear-with-the-maxdistance-value-from-database
+        result = []
+        for offer in offers:
+            dist = geo_distance.distance(offer.blurr_info.center.coordinates, center).km
+            if offer.visibility_radius + offer.blurr_info.radius < dist:
+                continue
+
+            result.append(offer)
+
+        return self._filter_date_time(result, time)
 
     async def _get_offer_with_checks(
         self, user: User, offer_id: PydanticObjectId
@@ -184,3 +233,70 @@ class OfferService:
         # TODO: notify potential partners that the offer has been closed
 
         await offer.delete()
+
+    async def request_to_join(
+        self, user: User, offer_id: PydanticObjectId, message: str
+    ):
+        offer = await self.get(offer_id)
+
+        if offer is None:
+            raise errors.UserAlreadyRequestedToJoin()
+
+        await offer.update(
+            Push(
+                {
+                    Offer.participants: Participant(
+                        id=user.id, status=ParticipantStatus.REQUESTED
+                    )
+                }
+            )
+        )
+
+        # TODO: send push notification to host with message
+
+    async def decline_request(
+        self, host: User, offer_id: PydanticObjectId, participant_id: PydanticObjectId
+    ):
+        self._set_participant_status(
+            host=host,
+            offer_id=offer_id,
+            participant_id=participant_id,
+            status=ParticipantStatus.DECLINED,
+        )
+
+    async def accept_request(
+        self, host: User, offer_id: PydanticObjectId, participant_id: PydanticObjectId
+    ):
+        self._set_participant_status(
+            host=host,
+            offer_id=offer_id,
+            participant_id=participant_id,
+            status=ParticipantStatus.ACCEPTED,
+        )
+
+    async def _set_participant_status(
+        self,
+        host: User,
+        offer_id: PydanticObjectId,
+        participant_id: PydanticObjectId,
+        status: ParticipantStatus,
+    ):
+        offer = await self.get(offer_id)
+        if not offer:
+            raise errors.OfferDoesNotExist()
+
+        if offer.user_info.id != host.id:
+            raise errors.UserDoesNotOwnOffer()
+
+        for p in offer.participants:
+            if p.id != participant_id:
+                continue
+
+            if p.status == status:
+                raise errors.ParticipantStatusUnchanged()
+
+            p.status = status
+            await offer.save()
+            break
+        else:
+            raise errors.UserIsNotParticipant()
