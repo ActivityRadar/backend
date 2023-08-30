591701bfbef0efeb4d0d0c3bfbb3d09cc5e55712
Anton Kriese <anton.kriese@gmx.de>
2023-08-30 12:21:46 +0200

    feat: more data output for offers
    namely status and creation time

--- backend/database/models/offers.py
+++ backend/database/models/offers.py
@@ -158,21 +158,18 @@ class OfferIn(OfferBase):
 
 class OfferWithParticipants(OfferBase):
     participants: list[Participant]
+    creation_date: Datetime
+    user_info: OfferCreatorInfo
+    blurr_info: LocationBlurrOut
+    status: OfferStatus
 
 
 class OfferOut(OfferWithParticipants):
     id: PydanticObjectId
-    user_info: OfferCreatorInfo
     location: OfferLocation | None
-    blurr_info: LocationBlurrOut
 
 
 class Offer(Document, OfferWithParticipants):
-    user_info: OfferCreatorInfo
-    creation_date: Datetime
-    status: OfferStatus
-    blurr_info: LocationBlurrOut
-
     class Settings:
         name = "offers"
         indexes = [
